401777fc76edc4e9dde91f687afe5c882b716afd
Dicky Ermawan S <wanasaja@gmail.com>
2018-11-21 13:40:35 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    public function showAll()
+    {
+        // $dataProductDariModel = Product::all();
+        return view('product.display',[
+            // 'products' => $dataProductDariModel
+        ]);
+
+    }
+
+    public function saveNew()
+    {
+
+    }
+}
---
+++ resources/views/product/display.blade.php
@@ -0,0 +1,1 @@
+<b>view product displaay siap</b>
